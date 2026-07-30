5e3dd6e0de1accf597d4cf2aa21bc29836205a5b
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-30 10:40:33 +0330

    Add class Repository

---
+++ src/main/java/com/learn/test_security_2/repository/RoleRepository.java
@@ -0,0 +1,20 @@
+package com.learn.test_security_2.repository;
+
+import com.learn.test_security_2.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+
+    Optional<Role> findByCode(String code);
+    Optional<Role> findByName(String name);
+    boolean existsByCode(String code);
+    boolean existsByName(String name);
+    List<Role> findAllByActiveTrue();
+
+}
