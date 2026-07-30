c4616b36781740207df3a778899189cdbfe51566
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-07-30 10:12:06 +0330

    first commit

---
+++ src/main/java/com/learn/test_security_2/TestSecurity2Application.java
@@ -0,0 +1,13 @@
+package com.learn.test_security_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSecurity2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSecurity2Application.class, args);
+    }
+
+}
