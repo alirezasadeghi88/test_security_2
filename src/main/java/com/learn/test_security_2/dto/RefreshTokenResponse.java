02bde6cc82d104fd34da5f277f33f8d03dbed2cd
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-20 10:03:09 +0330

    Add class RefreshTokenResponse

---
+++ src/main/java/com/learn/test_security_2/dto/RefreshTokenResponse.java
@@ -0,0 +1,18 @@
+package com.learn.test_security_2.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RefreshTokenResponse {
+    private String accessToken;
+    private String refreshToken;
+    private String tokenType;
+    private Long expiresIn;
+
+}
