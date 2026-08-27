6b75677583a76f90cf89d954862283049e68716e
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-27 20:45:06 +0330

    Add class TokenException

---
+++ src/main/java/com/learn/test_security_2/exception/TokenException.java
@@ -0,0 +1,7 @@
+package com.learn.test_security_2.exception;
+
+public class TokenException extends RuntimeException {
+    public TokenException(String message) {
+        super(message);
+    }
+}
