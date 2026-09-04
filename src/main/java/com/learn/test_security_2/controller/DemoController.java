76d0ddc1b535eeb40df663e8518c48867722fb3e
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-04 19:29:00 +0330

    Add class DemoController

--- src/main/java/com/learn/test_security_2/controller/DemoController.java
+++ src/main/java/com/learn/test_security_2/controller/DemoController.java
@@ -1,5 +1,6 @@
 package com.learn.test_security_2.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,14 @@ public class DemoController {
     public String publicApi() {
 
         return "Public API";
+
+    }
+
+    @GetMapping("/customer")
+    @PreAuthorize("hasRole('CUSTOMER')")
+    public String customer() {
+
+        return "Customer API";
 
     }
 }
