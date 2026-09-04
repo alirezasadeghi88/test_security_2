977b94ea92add82b10eb188c90c36a537dfc7897
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-04 19:27:51 +0330

    Add class DemoController

--- src/main/java/com/learn/test_security_2/controller/DemoController.java
+++ src/main/java/com/learn/test_security_2/controller/DemoController.java
@@ -1,9 +1,17 @@
 package com.learn.test_security_2.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/demo")
 public class DemoController {
+
+    @GetMapping("/public")
+    public String publicApi() {
+
+        return "Public API";
+
+    }
 }
