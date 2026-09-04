58c244155f8d2dd4f6b44ec6d721532c7bf39a31
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-04 19:17:56 +0330

    Add class AuthenticationController

--- src/main/java/com/learn/test_security_2/controller/AuthenticationController.java
+++ src/main/java/com/learn/test_security_2/controller/AuthenticationController.java
@@ -1,7 +1,14 @@
 package com.learn.test_security_2.controller;
 
+import com.learn.test_security_2.dto.LoginResponse;
+import com.learn.test_security_2.dto.RegisterRequest;
 import com.learn.test_security_2.service.AuthenticationService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<LoginResponse> register(
+            @Valid @RequestBody RegisterRequest request) {
+
+        LoginResponse response = authenticationService.register(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
 }
